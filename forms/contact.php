835dfbe8400765a6b47415e4c5f33802bd2ef769
nazril123-ind <supriatmanaril@gmail.com>
2025-02-09 16:48:08 +0700

    Add files via upload

---
+++ forms/contact.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $subject = htmlspecialchars($_POST["subject"]);
+    $message = htmlspecialchars($_POST["message"]);
+    
+    $to = "supriatmanaril@gmail.com";
+    $headers = "From: " . $email . "\r\n";
+    $headers .= "Reply-To: " . $email . "\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+    
+    $fullMessage = "Name: $name\n";
+    $fullMessage .= "Email: $email\n\n";
+    $fullMessage .= "Message:\n$message\n";
+    
+    if (mail($to, $subject, $fullMessage, $headers)) {
+        echo "<script>alert('Your message has been sent successfully!'); window.location.href='index.html';</script>";
+    } else {
+        echo "<script>alert('Failed to send message. Please try again later.'); window.location.href='index.html';</script>";
+    }
+} else {
+    echo "<script>alert('Invalid request.'); window.location.href='index.html';</script>";
+}
+?>
